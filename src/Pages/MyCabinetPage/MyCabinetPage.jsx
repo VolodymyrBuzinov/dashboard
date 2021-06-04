26e212a1c1e714988e0cbe46899e0d011299dac1
Tetiana-Yarmolenko <kylik_t_86_86@ukr.net>
2021-06-04 16:22:10 +0300

    add header in cabinet

--- src/Pages/MyCabinetPage/MyCabinetPage.jsx
+++ src/Pages/MyCabinetPage/MyCabinetPage.jsx
@@ -1,8 +1,10 @@
 import ExitButton from '../../Components/ExitButton/ExitButton';
+import HeaderPage from '../HeaderPage/HeaderPage'
 
 const MyCabinetPage = () => {
   return (
     <>
+      <HeaderPage />
       <ExitButton />
       <h1>Hello to MyCabinet</h1>
     </>
