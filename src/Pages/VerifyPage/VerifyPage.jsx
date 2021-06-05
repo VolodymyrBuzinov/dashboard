8709370b5bf0c4b6e8489e1d7fc63c49a8b56c0c
Andreyz11288 <andreyz11288@gmail.com>
2021-06-05 12:10:58 +0300

    переделал оформление с главной страницы на страницу верификации

--- src/Pages/VerifyPage/VerifyPage.jsx
+++ src/Pages/VerifyPage/VerifyPage.jsx
@@ -1,7 +1,11 @@
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-// import { NavLink } from 'react-router-dom';
 import { verify } from '../../Redux/Operations/authOperation';
+import s from './Verify.module.scss';
+import bgMobile from '../../Images/bg-mobile.png';
+import bgMobile2 from '../../Images/bg-mobile2.png';
+import pic1 from '../../Images/pic1.png';
+import pic2 from '../../Images/pic2.png';
 
 export default function VerifyPage() {
   const dispatch = useDispatch();
@@ -12,11 +16,20 @@ export default function VerifyPage() {
     onLogout();
   }, 5000);
   return (
-    <>
-      <h1>Ура почта подтверждена!!!</h1>
-      {/* <NavLink exact to="/">
-        Валим отсюда
-      </NavLink> */}
-    </>
+    <section className={s.login_section}>
+      <div className={s.login_container}>
+        <h1 className={s.login_title}>Questify</h1>
+        <p className={s.login_description}>
+          Congratulations, email verification was successful.
+        </p>
+        <p className={s.login_text}>
+          in 5 seconds you will be redirected to the Home page
+        </p>
+      </div>
+      <img src={pic1} alt="" className={s.login_loginPic} />
+      <img src={pic2} alt="" className={s.login_loginPicture} />
+      <img src={bgMobile} alt="" className={s.login_bgMobile} />
+      <img src={bgMobile2} alt="" className={s.login_bgMobile} />
+    </section>
   );
 }
