02a5f415f9e33b9f53b5d757302f0122909724f0
Tetiana-Yarmolenko <kylik_t_86_86@ukr.net>
2021-06-06 18:30:48 +0300

    fixed

--- src/Components/UserNameText/UserNameText.jsx
+++ src/Components/UserNameText/UserNameText.jsx
@@ -3,9 +3,10 @@ import s from './UserNameText.module.scss';
 const UserNameText = ({name}) => {
     return (
       <div>
-        <span className={s.userNameText}>{name}’s Quest Log</span>
-    </div>  
-    )
+        <span className={s.userNameText}>
+          {name}’s Quest Log
+          </span>
+    </div>  )
 }
 
 export default UserNameText
