bd9cdfb2a875439566b632e6fd73bfdbafe71d19
Yurii <streltsovyurii86@gmail.com>
2021-06-09 01:10:40 +0300

    Собрал в одно целое: 1. Добавил шаблон в список 2. Добавил запрос на создание 3. Подкорректировал условия рендера

--- src/Components/DashboardList/DashboardList.jsx
+++ src/Components/DashboardList/DashboardList.jsx
@@ -9,34 +9,32 @@ import todoOperations from '../../Redux/Operations/todosOperations';
 import todoSelectors from '../../Redux/Selectors/todosSelectors';
 import MenuDone from '../MenuDone/MenuDone.jsx';
 import TemplateTodo from '../TemplateTodo/TemplateTodo';
-//import { onClickBtnCreate } from '../../Redux/Actions/onClickBtnCreate-action';
-//import Button from '../Button/Button';
+import { onClickBtnCreate } from '../../Redux/Actions/onClickBtnCreate-action';
+import Button from '../Button/Button';
 import isVisibleTemplate from '../../Redux/Selectors/isVisibleSelector';
 import isEdit from '../../Redux/Selectors/editTodoSelector';
 
-
 const DashboardList = () => {
   const dispatch = useDispatch();
- /* const [todayList, setTodayList] = useState([]);
+  /* const [todayList, setTodayList] = useState([]);
   const [tomorrowList, setTomorrowList] = useState([]);
   const [doneList, setdoneList] = useState([]);
   const [challengeList, setChallengeList] = useState([]);*/
+  const isVisible = useSelector(isVisibleTemplate);
+  const edit = useSelector(isEdit);
 
   useEffect(() => {
     dispatch(todoOperations.fetchTodos());
-  }, [dispatch]);
+  }, [dispatch, isVisible]);
   const todos = useSelector(todoSelectors.getAllTodos);
-  
-  const isVisible = useSelector(isVisibleTemplate);
-  //const edit = useSelector(isEdit);
- 
-  /*const onClick = () => {
-    if(edit) return
+
+  const onClick = () => {
+    if (edit) return;
     dispatch(onClickBtnCreate(true));
-  };*/
+  };
 
   const today = new Date();
-  const tomorrow = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+  const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000);
 
   //const LIST_LEVEL = ['easy', 'normal', 'hard'];
   const todayList = [];
@@ -47,86 +45,105 @@ const DashboardList = () => {
   todos.map(todo => {
     if (!todo.done) {
       if (!todo.challenge) {
-        if (today.getFullYear() === new Date(Date.parse(todo.time)).getFullYear()
-          && today.getMonth() === new Date(Date.parse(todo.time)).getMonth()
-          && today.getDay() === new Date(Date.parse(todo.time)).getDay()) {
-          todayList.push(todo)
+        if (
+          today.getFullYear() ===
+            new Date(Date.parse(todo.time)).getFullYear() &&
+          today.getMonth() === new Date(Date.parse(todo.time)).getMonth() &&
+          today.getDay() === new Date(Date.parse(todo.time)).getDay()
+        ) {
+          todayList.push(todo);
           //setTodayList(todo)
         }
-        if (tomorrow.getFullYear() === new Date(Date.parse(todo.time)).getFullYear()
-          && tomorrow.getMonth() === new Date(Date.parse(todo.time)).getMonth()
-          && tomorrow.getDay() === new Date(Date.parse(todo.time)).getDay()) {
-          tomorrowList.push(todo)
+        if (
+          tomorrow.getFullYear() ===
+            new Date(Date.parse(todo.time)).getFullYear() &&
+          tomorrow.getMonth() === new Date(Date.parse(todo.time)).getMonth() &&
+          tomorrow.getDay() === new Date(Date.parse(todo.time)).getDay()
+        ) {
+          tomorrowList.push(todo);
           //setTomorrowList(todo)
         }
-      } else {challengeList.push(todo)//setChallenge(todo)
+      } else {
+        challengeList.push(todo); //setChallenge(todo)
       }
+    } else {
+      doneList.push(todo); //setdoneList(todo)
     }
-    else {doneList.push(todo)//setdoneList(todo)
-    } return { todayList, tomorrowList, doneList }
+    return { todayList, tomorrowList, doneList };
   });
   return (
     <>
       <main className={s.todoListMain}>
         <div className={s.todoListDiv}>
-          {todayList.length > 0 ? <>
-            <p className={s.todoListTitle}>TODAY</p>
-            <ul className={s.todoList}>
-              {!isVisible && (
-              <DashboardListItem>
-                <TemplateTodo isVisible={isVisible} />
-              </DashboardListItem>
-              )}
-              {todayList.map(
-                ({ title, _id, time, category, difficulty, challenge }) =>
-                  <li key={_id}>
-                    <DashboardListItem
-                      category={category}
-                      difficulty={difficulty}
-                      title={title}
-                      id={_id}
-                      time={time}
-                  ></DashboardListItem>
-                  </li>
-              )}
-            </ul></>
-            : null}
-          {tomorrowList.length > 0 ? <> 
-            <p className={s.todoListTitle}>TOMORROW</p>
-            <ul className={s.todoList}>
-              {tomorrowList.map(
-                ({ title, _id, time, category, difficulty, challenge }) =>
-                    <li key={_id} >
-                    <DashboardListItem
-                      category={category}
-                      difficulty={difficulty}
-                      title={title}
-                      id={_id}
-                      time={time}
-                  ></DashboardListItem>
-                  </li>
-              )}
-            </ul></>
-            : null}
-          {challengeList.length > 0 ? <> 
-            <p className={s.todoListTitle}>CHALLENGES</p>
-            <ul className={s.todoList}>
-              {challengeList.map(
-                ({ title, _id, time, category, difficulty, challenge }) =>
-                    <li key={_id} >
-                    <DashboardListItem
-                      category={category}
-                      difficulty={difficulty}
-                      title={title}
-                      id={_id}
-                      time={time}
-                  ></DashboardListItem>
-                  </li>
-              )}
-            </ul></>
-            : null}
-            <MenuDone todos={doneList}/>
-          </div>
+          {todayList.length > 0 || isVisible ? (
+            <>
+              <p className={s.todoListTitle}>TODAY</p>
+              <ul className={s.todoList}>
+                {isVisible && (
+                  <DashboardListItem key="template">
+                    <TemplateTodo isVisible={isVisible} />
+                  </DashboardListItem>
+                )}
+                {todayList.length > 0 &&
+                  todayList.map(
+                    ({ title, _id, time, category, difficulty, challenge }) => (
+                      <DashboardListItem key={_id}>
+                        <TemplateTodo
+                          category={category}
+                          difficulty={difficulty}
+                          title={title}
+                          time={time}
+                          challenge={challenge}
+                        />
+                      </DashboardListItem>
+                    ),
+                  )}
+              </ul>
+            </>
+          ) : null}
+          {tomorrowList.length > 0 ? (
+            <>
+              <p className={s.todoListTitle}>TOMORROW</p>
+              <ul className={s.todoList}>
+                {tomorrowList.map(
+                  ({ title, _id, time, category, difficulty, challenge }) => (
+                    <DashboardListItem key={_id}>
+                      <TemplateTodo
+                        category={category}
+                        difficulty={difficulty}
+                        title={title}
+                        time={time}
+                        challenge={challenge}
+                      />
+                    </DashboardListItem>
+                  ),
+                )}
+              </ul>
+            </>
+          ) : null}
+          {challengeList.length > 0 ? (
+            <>
+              <p className={s.todoListTitle}>CHALLENGES</p>
+              <ul className={s.todoList}>
+                {challengeList.map(
+                  ({ title, _id, time, category, difficulty, challenge }) => (
+                    <li key={_id}>
+                      <DashboardListItem
+                        category={category}
+                        difficulty={difficulty}
+                        title={title}
+                        id={_id}
+                        time={time}
+                      ></DashboardListItem>
+                    </li>
+                  ),
+                )}
+              </ul>
+            </>
+          ) : null}
+          <MenuDone todos={doneList} />
+        </div>
+        <Button content="icon-plus" type="button" onClick={onClick} />
       </main>
     </>
   );
@@ -134,7 +151,7 @@ const DashboardList = () => {
 
 export default DashboardList;
 
-  /**<div
+/**<div
         className={
           challenge
             ? `${style.TemplateTodo__challenge} ${style.TemplateTodo__group}`
