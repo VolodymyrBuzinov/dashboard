882d9789bb6a81910b01f5d211a023b7b4007667
AnastaciaZ <zaikina.anastacia@gmail.com>
2021-06-09 21:55:31 +0300

    addToastContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Router from './Router/Router';
+import { ToastContainer, Flip, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <Router />
+       <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        transition={Zoom}
+        />
     </div>
   );
 }
